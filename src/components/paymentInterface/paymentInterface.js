849084fd9a5ab9ff281e829e4c691a123bf1b899
Michael Perez <mperez6473@gmail.com>
2021-04-02 15:15:25 -0500

    paymentInterface has handleChanges for it's inputs as well as restrictions on number limits. fors changed to htmlFor, class to className

--- src/components/paymentInterface/paymentInterface.js
+++ src/components/paymentInterface/paymentInterface.js
@@ -1,30 +1,43 @@
-export default function PaymentInterface() {
+export default function PaymentInterface(props) {
+  function sendCardInfo(e) {
+    e.preventDefault();
+    console.log("address form sent: ", props.cardInfo);
+  }
+  const handleChanges = (e) => {
+    e.preventDefault();
+    const updatedCardInfo = {
+      ...props.cardInfo["cardInfo"],
+      [e.target.id]: e.target.value
+    };
+    return props.cardInfo["setCardInfo"](updatedCardInfo);
+  };
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col-xs-12 col-md-4">
+        <div className="col-xs-12 col-md-9">
           <div className="panel panel-default">
             <div className="panel-heading">
               <h3 className="panel-title">Payment Details</h3>
               <div className="checkbox pull-right">
                 <label>
-                  <input type="checkbox" />
+                  <input onChange={handleChanges} type="checkbox" />
                   Remember
                 </label>
               </div>
             </div>
             <div className="panel-body">
-              <form>
+              <form onSubmit={sendCardInfo}>
                 <div className="form-group">
-                  <label for="cardNumber">CARD NUMBER</label>
+                  <label htmlFor="cardNumber">CARD NUMBER</label>
                   <div className="input-group">
                     <input
+                      onChange={handleChanges}
                       type="text"
                       className="form-control"
                       id="cardNumber"
                       placeholder="Valid Card Number"
                       required
-                      autofocus
                     />
                     <span className="input-group-addon">
                       <span className="glyphicon glyphicon-lock"></span>
@@ -34,36 +47,47 @@ export default function PaymentInterface() {
                 <div className="row">
                   <div className="col-xs-7 col-md-7">
                     <div className="form-group">
-                      <label for="expityMonth">EXPIRY DATE</label>
+                      <label htmlFor="expityMonth">EXPIRY DATE</label>
                       <div className="col-xs-6 col-lg-6 pl-ziro">
                         <input
-                          type="text"
+                          onChange={handleChanges}
+                          type="number"
                           className="form-control"
                           id="expityMonth"
                           placeholder="MM"
                           required
+                          maxLength="2"
+                          max="12"
+                          min="1"
                         />
                       </div>
                       <div className="col-xs-6 col-lg-6 pl-ziro">
                         <input
-                          type="text"
+                          onChange={handleChanges}
+                          type="number"
                           className="form-control"
                           id="expityYear"
                           placeholder="YY"
                           required
+                          maxLength="2"
+                          max={new Date().getFullYear() + 14}
+                          min={new Date().getFullYear()}
                         />
                       </div>
                     </div>
                   </div>
                   <div className="col-xs-5 col-md-5 pull-right">
                     <div className="form-group">
-                      <label for="cvCode">CV CODE</label>
+                      <label htmlFor="cvCode">CV CODE</label>
                       <input
+                        onChange={handleChanges}
                         type="password"
                         className="form-control"
                         id="cvCode"
                         placeholder="CV"
                         required
+                        autoComplete="false"
+                        maxLength="3"
                       />
                     </div>
                   </div>
