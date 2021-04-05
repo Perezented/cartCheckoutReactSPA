48ad10d003b7775e55f72d838e9fd5bb0a983abc
Michael Perez <mperez6473@gmail.com>
2021-04-05 07:22:31 -0500

    Have the cart total being shown on the checkout screen

--- src/components/shoppingCart/panelFooter.js
+++ src/components/shoppingCart/panelFooter.js
@@ -1,19 +1,12 @@
 import { Link } from "react-router-dom";
 
 export default function PanelFooter(cart) {
-  const actualCart = cart.cart;
-  actualCart.total = 0;
-  actualCart.map((cartItem) => {
-    const productPrice = cartItem.productPrice;
-    const quantity = cartItem.quantity;
-    actualCart.total += productPrice * quantity;
-  });
   return (
     <div className="panel-footer">
       <div className="row text-center">
         <div className="col-xs-9">
           <h4 className="text-right">
-            Total <strong>${actualCart.total}</strong>
+            Total <strong>${cart.cart.total}</strong>
           </h4>
         </div>
         <div className="col-xs-3">
