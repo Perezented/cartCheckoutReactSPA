ba2470df1543ad25c537532535d8dcd7fc23880d
Michael Perez <mperez6473@gmail.com>
2021-04-05 07:00:33 -0500

    Cart is started with some products being mocked with a JSON file

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import AddressForm from "./components/addressForm/addressForm";
 import PaymentInterface from "./components/paymentInterface/paymentInterface";
 import ShoppingCart from "./components/shoppingCart/shoppingCart";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import products from "./config/config.json";
 import ConfirmationPage from "./components/confirmationPage/confirmationPage";
 
+// dictionary to house current cart and old cart
+const dict = {};
+const old_cart = {};
 function App() {
+  // Start of 'adding' items to cart
+  const cartItem0 = products.Products.product0;
+  const cartItem1 = products.Products.product1;
+  // have some sort of quantity to display for each product
+  cartItem0.quantity = 1;
+  cartItem1.quantity = 2;
+  // cart state set to the mock items
+  const [cart, setCart] = useState([cartItem0, cartItem1]);
+  // cardInfo
   const [cardInfo, setCardInfo] = useState({
     cardNumber: "",
     expMonth: "",
     expYear: "",
     cvCode: ""
   });
+  // Address info for user
   const [addressInfo, setAddressInfo] = useState({
     address: "",
     address2: "",
@@ -36,10 +50,10 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/">
-              <ShoppingCart />
+              <ShoppingCart cartInfo={{ cart, setCart }} />
             </Route>
             <Route exact path="/cart">
-              <ShoppingCart />
+              <ShoppingCart cartInfo={{ cart, setCart }} />
             </Route>
             <Route exact path="/confirmation">
               <ConfirmationPage />
