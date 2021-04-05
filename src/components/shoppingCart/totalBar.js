80cc7b4bcf662a8a79dfed805291783bcc7a29a6
Michael Perez <mperez6473@gmail.com>
2021-04-05 07:02:11 -0500

    Total bar for the cart separated into its own component as well

---
+++ src/components/shoppingCart/totalBar.js
@@ -0,0 +1,16 @@
+export default function TotalBar() {
+  return (
+    <div className="row">
+      <div className="text-center">
+        <div className="col-xs-9">
+          <h6 className="text-right">Added items?</h6>
+        </div>
+        <div className="col-xs-3">
+          <button type="button" className="btn btn-default btn-sm btn-block">
+            Update cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
