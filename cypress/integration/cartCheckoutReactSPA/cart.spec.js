12ccefa1377654466b05bacbaaeb05fef287dac7
Michael Perez <mperez6473@gmail.com>
2021-04-07 14:37:33 -0500

    tests for opening cart page and removing from the cart

--- cypress/integration/cartCheckoutReactSPA/cart.spec.js
+++ cypress/integration/cartCheckoutReactSPA/cart.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context("Window", () => {
+context("Should contain cart page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
@@ -11,6 +11,19 @@ context("Window", () => {
   it("Should contain nav items", () => {
     cy.contains(/cart/i);
     cy.contains(/checkout/i);
+  });
+
+  it("Should have a update cart button", () => {
+    cy.contains(/update cart/i);
+  });
+  it("Should have a checkout button", () => {
+    cy.contains(/checkout/i);
+  });
+});
+
+context("Should remove items from the cart", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
   });
 
   it("Should contain products in the cart", () => {
